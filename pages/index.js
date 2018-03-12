c9af00c6cbc1b85362c0397c29e42814dbe4f87b
GendSmith <1439786496@qq.com>
2018-03-12 22:32:16 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -5,42 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
   },
 
   /**
@@ -51,16 +16,25 @@ Page({
   },
 
   /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    
+  },
+  onCet4: function () {
+      wx.navigateTo({
+        url: 'word/cet4/cet4',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+  },
+  onCet6:function () {
+    wx.navigateTo({
+      url: 'word/cet6/cet6',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   }
 })
